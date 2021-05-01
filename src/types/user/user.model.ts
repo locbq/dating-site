253902c69b1f8a8d6c62b5d47fff782395132b2b
locbq <locbq.cyclonedart@gmail.com>
2021-05-01 09:43:00 +0700

    add user list default, add user type

---
+++ src/types/user/user.model.ts
@@ -0,0 +1,12 @@
+export interface UserModel {
+  id: number;
+  name: string;
+  email: string;
+  gender: string;
+  age: number;
+  personalityType: string;
+  favoriteOs: string,
+  minSeekingAge: number,
+  maxSeekingAge: number,
+  passsword: string,
+}
