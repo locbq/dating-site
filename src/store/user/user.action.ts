58cc27293144c9e1595d702be5162b4ba46b6c0a
locbq <locbq.cyclonedart@gmail.com>
2021-05-03 14:11:30 +0700

    add default data

--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -4,7 +4,7 @@ import {
 } from 'types/action/action.model';
 import { UserModel } from 'types/user/user.model';
 
-export const addUsers = (users: any[]): UserActionTypes => ({
+export const addUsers = (users: UserModel[]): UserActionTypes => ({
   type: ADD_USERS,
   payload: users,
 });
